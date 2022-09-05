8017e66a2f006ef3a6bf043d17cbb0db3fad5ab6
Davide Nastri <d.nastri@gmail.com>
2022-09-05 23:20:25 +0200

    Completed State Management 2 and 3

--- L3. State Management/2. Passing Data II/Starter/src/App.js
+++ L3. State Management/2. Passing Data II/Starter/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import logo from "./logo.svg";
 
+import Dashboard from "./Dashboard";
+
 // Display a list of movies where each movie contains a list of users that favorited it.
 // For detailed instructions, refer to Instructions.md.
 
@@ -94,6 +96,17 @@ const movies = {
 };
 
 const App = () => {
+  let usersByMovie = {};
+
+  profiles.forEach((profile) => {
+    const movieID = profile.favoriteMovieID;
+    if (usersByMovie[movieID]) {
+      usersByMovie[movieID].push(profile.userID);
+    } else {
+      usersByMovie[movieID] = [profile.userID];
+    }
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -101,6 +114,9 @@ const App = () => {
         <h1 className="App-title">ReactND - Coding Practice</h1>
       </header>
       <h2>How Popular is Your Favorite Movie?</h2>
+      <ul>
+        <Dashboard usersByMovie={usersByMovie} movies={movies} users={users} />
+      </ul>
     </div>
   );
 };
