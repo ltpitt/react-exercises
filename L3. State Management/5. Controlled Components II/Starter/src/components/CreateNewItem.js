20769fc9b4d8ec35201f95cba21077a17ed22b8a
Davide Nastri <d.nastri@gmail.com>
2022-09-06 23:08:22 +0200

    Completed exercise L3, 5

---
+++ L3. State Management/5. Controlled Components II/Starter/src/components/CreateNewItem.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+const CreateNewItem = ({ onAddItem }) => {
+  const [value, setValue] = useState("");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const addItem = (event) => {
+    event.preventDefault();
+    onAddItem(value);
+    setValue("");
+  };
+
+  const isInputEmpty = () => value === "";
+
+  return (
+    <form onSubmit={addItem}>
+      <input
+        type="text"
+        placeholder="Enter New Item"
+        value={value}
+        onChange={handleChange}
+      />
+      <button disabled={isInputEmpty()}>Add</button>
+    </form>
+  );
+};
+
+CreateNewItem.propTypes = {
+  onAddItem: PropTypes.func.isRequired,
+};
+
+export default CreateNewItem;
